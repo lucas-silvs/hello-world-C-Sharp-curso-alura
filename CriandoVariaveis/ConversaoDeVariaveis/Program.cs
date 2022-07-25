e5251af2e4d61346f451201ba423519c80ea60e7
Lucas Santos <lucas.parkl@outlook.com>
2022-07-25 20:22:15 -0300

    projeto teste das linguagem C#: - estrutura de repetições - inputline via console - criação de variaveis

---
+++ CriandoVariaveis/ConversaoDeVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿class Programa2
+{
+    static void Main(String[] arg)
+    {
+        int idade = 12;
+        
+        Console.WriteLine("Criando variaveis inteiras");
+        Console.WriteLine(idade);
+        
+        Console.WriteLine("Criando variaveis double");
+        Console.WriteLine("Digite uma idade: ");
+        idade = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("digite um número decimal");
+        double numeroDecimal = double.Parse(Console.ReadLine());
+        Console.WriteLine(idade);
+        Console.WriteLine(numeroDecimal);
+
+
+
+    }
+    
+}
---
+++ CriandoVariaveis/fatorial/Program.cs
@@ -0,0 +1,20 @@
+﻿class fatorial
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Digite um número para ser o valor do fatorial");
+        int entrada = Int32.Parse(Console.ReadLine());
+        int total = entrada;
+        while (entrada != 1)
+        {
+            entrada -= 1;
+            total = total * entrada;
+        }
+        
+        Console.WriteLine("Valor da fatorial: " + total);
+
+        Console.ReadLine();
+
+    }
+    
+}
